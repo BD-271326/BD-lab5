3c595ef373a2c5d556836fbb0d8c42bb1e2b821d
Olechno <m.olechno11@gmail.com>
2018-04-18 17:51:26 +0200

    initial

---
+++ src/bd/OraConn.java
@@ -0,0 +1,31 @@
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class OraConn {
+
+	public static Connection open() {
+		try {
+			Connection connection = DriverManager.getConnection("jdbc:oracle:thin:@ora3.elka.pw.edu.pl:1521:ora3inf",
+					"molechno", "molechno");
+			System.out.println("Polaczenie jest otwarte");
+			return connection;
+		} catch (SQLException ex) {
+			System.out.println("Nie mozna otworzyc polaczenia! Error Code: "+ ex.getErrorCode() + "; " + ex.getMessage());
+			return null;
+		}
+	}
+
+	public static void close(Connection connection) {
+		try {
+			connection.close();
+			System.out.println("Polaczenie jest zamkniête");
+		} catch (SQLException ex) {
+			System.out.println("Nie mozna zamknac polaczenia! Error code: " + ex.getErrorCode() + "; "
+					+ ex.getMessage());
+		}
+	}
+
+}
---
+++ src/bd/Test.java
@@ -0,0 +1,19 @@
+package bd;
+
+import java.sql.Connection;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			 System.out.println("OK");
+		} catch (ClassNotFoundException ex) {
+			System.out.println("Error: " + ex.getMessage() + " not found");
+		}
+		Connection connection=OraConn.open();
+		OraConn.close(connection);
+	}
+
+}
